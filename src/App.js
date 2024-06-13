15c8e8668e30f0bd1b44d67001f8d31a0b33d57b
Anurag <anurag.131092@gmail.com>
2024-06-13 14:52:20 +0530

    v1.2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, {useRef, useState, Suspense, useEffect} from 'react'
+import React, {useRef, useState, Suspense} from 'react'
 import { extend, Canvas, useFrame, useThree } from '@react-three/fiber'
-import { SwatchesPicker } from 'react-color'
-import {useGLTF, SoftShadows, useHelper, PerspectiveCamera, Html} from '@react-three/drei'
+import { HuePicker } from 'react-color'
+import {useGLTF} from '@react-three/drei'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import * as THREE from 'three'
+import { Model } from "./Model";
 import {l} from "./helpers"
 // Debug
 import { useControls } from "leva"
 import { Stats } from "@react-three/drei"
-import {MeshBasicMaterial, MeshStandardMaterial} from "three";
-import {Model1, Model2} from "./Model";
 
 // Make OrbitControls known as <orbitControls />
 extend({ OrbitControls })
@@ -53,29 +52,6 @@ const CameraControls = () => {
 
   return <orbitControls ref={controls} args={[camera, domElement]} />
 }
-, Box = props => {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    if(mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  })
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'green' : 'orange'} />
-    </mesh>
-  )
-}
 , PointLightWithHelper = ({ color, position, visible, intensity }) => {
   const lightProps = { color, position, intensity }
   return (
@@ -87,107 +63,24 @@ const CameraControls = () => {
     </pointLight>
   )
 }
-, Model = (props) => {
-  const { nodes, materials } = useGLTF(model)
-  return (
-    <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          <mesh castShadow material-color="yellow" geometry={nodes.baseballCap.geometry} material={materials.baseballCap} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-          <mesh castShadow material-color="red" geometry={nodes.baseballCap_1.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-          <mesh castShadow material-color="blue" geometry={nodes.plastic.geometry} material={materials.plastic} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
-          <mesh castShadow material-color="orange" geometry={nodes.plastic_1.geometry} material={materials.plastic.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-          <mesh castShadow material-color="aquamarine" geometry={nodes.baseballCap_2.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-          <mesh castShadow material-color="purple" geometry={nodes.blinn1SG.geometry} material={materials.blinn1SG.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-          <mesh castShadow material-color={0xff0fe7} geometry={nodes.baseballCap_3.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-        </group>
-      </group>
-    </group>
-  )
-},
-Floor = (props) => {
+, Floor = (props) => {
   return (
     <mesh {...props} rotation-x={-Math.PI / 2} receiveShadow>
       <circleGeometry args={[100]} />
-      <meshStandardMaterial color={0xB5D0EC}/>
+      <meshStandardMaterial side={THREE.DoubleSide} transparent={true} color={0xB5D0EC} opacity={.4}/>
     </mesh>
   )
-},
-MyCamera = ({ makeDefault, animate, label, near = 10, far = 500, position = [0, 10, 5] }) => {
-  const ref = useRef()
-  
-  const {
-    camera,
-    // gl: { domElement },
-    gl,
-    scene
-  } = useThree()
-  // console.log(useThree())
-  // const props = useControls(label, {
-  //   near,
-  //   far,
-  //   fov: 25
-  // })
-  const props = {
-    near,
-    far,
-    fov: 25
-  }
-  
-  const t = new THREE.Vector3(0, 0, 0)
-  
-  useFrame((state) => {
-    if(ref.current && gl){
-      gl.render(scene, ref.current)
-    }
-    if (animate) {
-      // animate(ref.current, state)
-      ref.current.lookAt(t)
-    }
-  })
-  
-  useEffect(() => {
-    // useStore.setState({
-    //   [`${label}Cam`]: ref
-    // })
-
-    ref.current.lookAt(t)
-  }, [label])
-  
-  // useHelper(ref, THREE.CameraHelper)
-  
-  return (
-    <PerspectiveCamera makeDefault={makeDefault} {...props} position={position} ref={ref}>
-      <Html className="label">{label}</Html>
-    </PerspectiveCamera>
-  )
-},
-Spinner = (props) => {
-  // l(text);
-  return (
-    <div style={{ display: "flex", alignItems:"center"}}>
-      <span className="loader" />
-      <span>&nbsp;{props.text}</span>
-    </div>
-  );
 }
 
-
-const CAMERA_DEFAULT_POS = [0, 50, 50]
-const model = '/baseball_cap.glb';
-
-// const ColorPicker = ({ color, onChange }) => {
-//   // console.log("colorPicker", color);
-//   return (
-//     <input type="color" value={color} onChange={onChange} />
-//   );
-// }
-let modelLoaded = false
+const CAMERA_DEFAULT_POS = [0, 10, 50],
+ model = '/baseball_cap.glb';
 
 export default function App() {
   const [colorCrown, setColorCrown] = useState("white");
   const [colorBrim, setColorBrim] = useState("hotpink");
   const [colorTop, setColorTop] = useState("green");
+  const [colorStrip, setColorStrip] = useState("yellow");
+  const [colorUnder, setColorUnder] = useState("aquamarine");
   
   const canvasRef = useRef()
   const helpers = false
@@ -195,258 +88,98 @@ export default function App() {
   //   helpers: false,
   // })
   useGLTF.preload(model)
-
-  
-  const [currImg, setCurrImg] = useState(null)
-  const [currImg2, setCurrImg2] = useState(null)
-  const [currImg3, setCurrImg3] = useState(null)
-  const [currImg4, setCurrImg4] = useState(null)
-  const [drawingImages, setDrawingImages] = useState(true)
-  const [currCam, setCurrCam] = useState(null)
-  const [message, setMessage] = useState("Loading model..")
-  let sleepSetTimeout_ctrl;
-  
-  function sleep(ms) {
-    clearInterval(sleepSetTimeout_ctrl);
-    return new Promise(resolve => sleepSetTimeout_ctrl = setTimeout(resolve, ms));
-  }
-  
-  const drawImg = function(){
-    l("Drawing:", currCam, "Setting image..")
-    const img = canvasRef.current.toDataURL("image/png");
-    switch(currCam) {
-      case "A":
-        setCurrImg(img)
-        setCurrCam("B")
-        break;
-  
-      case "B":
-        setCurrImg2(img)
-        setCurrCam("C")
-        break;
-  
-      case "C":
-        setCurrImg3(img)
-        setCurrCam("D")
-        break;
-        
-      default:
-        setCurrImg4(img)
-        setDrawingImages(false)
-        break;
-    }
-  }
-  
-  useEffect(() => {
-    l("drawingImages", drawingImages)
-  }, [drawingImages])
-  
-  const drawAll = async () => {
-    setDrawingImages(true)
-    setCurrCam("A")
-    // await sleep(300)
-    // setCurrCam("B")
-    // await sleep(300)
-    // setCurrCam("C")
-    // await sleep(300)
-    // setCurrCam("D")
-  }
-  
-  useEffect(() => {
-    l("currCam:", currCam)
-    if(!currCam) return
-    const doDraw = async() =>{
-      await sleep(50)
-      drawImg(currCam)
-    }
-    
-    doDraw()
-  }, [currCam])
-  
-  const h = window.innerHeight / 4,
-        w = window.innerWidth / 4
-  
-  const wasCalled = useRef(false);
-  /*
-  useEffect(() => {
-    if(wasCalled.current) return;
-    wasCalled.current = true;
-    
-    /!* CODE THAT SHOULD RUN ONCE *!/
-    
-  }, []);*/
   
   return (<>
-    {/*<div style={{position: "absolute", left: 200, zIndex: 2}}>
-      <SwatchesPicker
-        width={200}
-        height={150}
-        color={ color }
-        onChangeComplete={ (color, event) => {
-          l(color, event)
-          setColor(color.hex)
-          sleep(50).then(() => {
-            drawAll()
-          })
-        }}
-      />
-      <button onClick={drawAll}>Draw</button>
-    </div>
-    
-    <div style={{ opacity: 1, display: "flex", flexDirection:"column", position: "absolute", left: 0, top: 0, zIndex: 1,
-      // background: "rgba(0, 0, 0, .4)",
-      background: "white",
-      pointerEvents: "none"}}>
-      <div style={{ display: "flex"}}>
-        <img src={currImg} alt="" width={w} height={h}/>
-        <img src={currImg2} alt="" width={w} height={h}/>
-      </div>
-      <div style={{ display: "flex"}}>
-        <img src={currImg3} alt="" width={w} height={h}/>
-        <img src={currImg4} alt="" width={w} height={h}/>
-      </div>
-    </div>*/}
-    <div className="ctn-main">
-      {/*<div className="canvas">
-        <Canvas
-          className="ctn-hidden"
-          gl={{ preserveDrawingBuffer: true }}
-          ref={canvasRef}
-          shadows
-          camera={{ position: [50, 30, 50] }}>
-          <ambientLight intensity={.25} />
-          <PointLightWithHelper
-            visible={helpers}
-            color={0xffffff}
-            intensity={.75}
-            position={[70, 50, 25]}/>
-          <PointLightWithHelper
-            visible={helpers}
-            color={0xffffff}
-            intensity={.75}
-            position={[-70, 50, -25]}/>
-          <pointLight position={[0, 100, 0]} castShadow={true}/>
-          <CameraControls />
-          <Suspense fallback={null}>
-            <Model1
-              onLoad={() => {
-                l("cap 1 loaded")
-              }}
-              colorCrown={colorCrown}
-              colorBrim={colorBrim}
-              colorTop={colorTop} scale={[.2, .2, .2]}/>
-          </Suspense>
-          <Floor position={[0, -10, 0]} />
-        </Canvas>
-      </div>*/}
-      <div className="screenshots">
-        <div className="ctn-parts-outer">
-          <h2>Cap Parts</h2>
-          <div className="ctn-parts">
-            <div>
-              <h4>Crown</h4>
-              <SwatchesPicker
-                width={200}
-                height={100}
-                color={ colorCrown }
-                onChangeComplete={ (color, event) => {
-                  l(color, event)
-                  setColorCrown(color.hex)
-                  sleep(50).then(() => {
-                    drawAll()
-                  })
-                }}
-              />
-            </div>
-            <div>
-              <h4>Brim</h4>
-              <SwatchesPicker
-                width={200}
-                height={100}
-                color={ colorBrim }
-                onChangeComplete={ (color, event) => {
-                  l(color, event)
-                  setColorBrim(color.hex)
-                  sleep(50).then(() => {
-                    drawAll()
-                  })
-                }}
-              />
-            </div>
-            <div>
-              <h4>Top Button</h4>
-              <SwatchesPicker
-                width={200}
-                height={100}
-                color={ colorTop }
-                onChangeComplete={ (color, event) => {
-                  l(color, event)
-                  setColorTop(color.hex)
-                  sleep(50).then(() => {
-                    drawAll()
-                  })
-                }}
-              />
-            </div>
-          </div>
+    <div className="ctn-parts-outer">
+      <h3>Drag around with the mouse to change viewing angle.</h3>
+      <div className="ctn-parts">
+        <h3>Change cap part colors below:</h3>
+        <div>
+          <h4>Crown</h4>
+          <HuePicker
+            width={200}
+            color={ colorCrown }
+            onChangeComplete={ (color, event) => {
+              l(color, event)
+              setColorCrown(color.hex)
+            }}
+          />
         </div>
-        <div className="views">
-          {drawingImages && <div className="curtain">
-            <Spinner text={message} />
-          </div>}
-          {!drawingImages && <><div className="inner">
-            <div>
-              <h4>Left</h4>
-              <img src={currImg} alt="Left"/>
-            </div>
-            <div>
-              <h4>Right</h4>
-              <img src={currImg2} alt="Right"/>
-            </div>
-          </div>
-            <div style={{height: 50}}></div>
-            <div className="inner">
-            <div>
-            <h4>Back</h4>
-            <img src={currImg3} alt="Back" />
-            </div>
-            <div>
-            <h4>Front</h4>
-            <img src={currImg4} alt="Front" />
-            </div>
-            </div></>}
+        <div>
+          <h4>Brim</h4>
+          <HuePicker
+            width={200}
+            color={ colorBrim }
+            onChangeComplete={ (color, event) => {
+              l(color, event)
+              setColorBrim(color.hex)
+            }}
+          />
         </div>
+        <div>
+          <h4>Top Button</h4>
+          <HuePicker
+            width={200}
+            color={ colorTop }
+            onChangeComplete={ (color, event) => {
+              l(color, event)
+              setColorTop(color.hex)
+            }}
+          />
+        </div>
+        <div>
+          <h4>Back Strip</h4>
+          <HuePicker
+            width={200}
+            color={ colorStrip }
+            onChangeComplete={ (color, event) => {
+              l(color, event)
+              setColorStrip(color.hex)
+            }}
+          />
+        </div>
+        <div>
+          <h4>Underwire</h4>
+          <HuePicker
+            width={200}
+            color={ colorUnder }
+            onChangeComplete={ (color, event) => {
+              l(color, event)
+              setColorUnder(color.hex)
+            }}
+          />
+        </div>
+      </div>
+      <div id="ctn-about">
+        <h2>Cap color configurator</h2>
+        <div>by&nbsp;
+          <a href="http://envisagecyberart.in" target="_blank">Anurag Srivastava</a><br />
+          <a href="http://envisagecyberart.in/projects/threejs-experiments" target="_blank">More 3D projects</a>
+        </div>
+        <a href="https://www.upwork.com/o/profiles/users/~01d929751d145a05ea/" target="_blank">
+          <img src="images/upwork.png" alt="" />
+        </a>
+        <a href="https://www.guru.com/freelancers/anurag-srivastava-27" target="_blank">
+          <img src="images/guru.png" alt="" />
+        </a>
+        <a href="mailto:anurag.131092@gmail.com&Subject=New Work Proposal">
+          <img src="images/gmail.png" alt="" />
+        </a>
+        <a href="https://stackoverflow.com/users/7867822/anurag-srivastava" target="_blank">
+          <img src="images/so.png" alt="" />
+        </a>
+        <a href="https://github.com/anuragsr" target="_blank">
+          <img src="images/github.png" alt="" />
+        </a>
       </div>
     </div>
     
     <Canvas
-      className="ctn-hidden"
       gl={{ preserveDrawingBuffer: true }}
       ref={canvasRef}
       shadows
       camera={{ position: CAMERA_DEFAULT_POS }}>
-      <color attach="background" args={["#B5D0EC"]} />
       <ambientLight intensity={.25} />
-      <MyCamera
-        // label="A"
-        position={[120, 20, 0]}
-        makeDefault={currCam === "A"}
-      />
-      <MyCamera
-        label="B"
-        position={[-120, 20, 0]}
-        makeDefault={currCam === "B"}
-      />
-      <MyCamera
-        label="C"
-        position={[0, 50, -100]}
-        makeDefault={currCam === "C"}
-      />
-      <MyCamera
-        label="D"
-        position={[0, 50, 100]}
-        makeDefault={currCam === "D"}
-      />
       <PointLightWithHelper
         visible={helpers}
         color={0xffffff}
@@ -458,43 +191,25 @@ export default function App() {
         intensity={.75}
         position={[-70, 50, -25]}/>
       
-      {/*<PointLightWithHelper
-        visible={helpers}
-        color={0xffffff}
-        intensity={.5}
-        position={[0, 100, 0]}/>*/}
       {helpers && <>
         <Stats showPanel={0} className="stats" />
         <gridHelper args={[1000, 100]} />
         <axesHelper args={[500]} />
       </>}
-      {/*<spotLight castShadow={true} position={[10, 10, 10]} angle={0.15} penumbra={1} />*/}
       <pointLight position={[0, 100, 25]} castShadow={true}/>
-      {/*<directionalLight position={[0, 50, 0]} castShadow={true}/>*/}
       <CameraControls />
-      {/*<SoftShadows {...config} />*/}
-      {/*<fog attach="fog" args={["white", 0, 40]} />*/}
       <Suspense fallback={null}>
-        {/*<Model position={[-20, 0, 0]} scale={[.2, .2, .2]}/>*/}
-        <Model1
-          onLoad={() => {
-            l("cap 2 loaded")
-            l(modelLoaded)
-            if(!modelLoaded) {
-              setMessage("Generating..")
-              drawAll()
-            }
-            modelLoaded = true
-            // if(wasCalled.current) drawAll()
-          }}
+        <Model
           colorCrown={colorCrown}
           colorBrim={colorBrim}
           colorTop={colorTop}
+          colorStrip={colorStrip}
+          colorUnder={colorUnder}
           rotation={[ .2, 0, 0]}
-          position={[0, 0, 0]} scale={[.2, .2, .2]}/>
+          position={[0, 0, 0]}
+          scale={[.2, .2, .2]}/>
       </Suspense>
       <Floor position={[0, -10, 0]} />
-      {/*<Box position={[0, 0, 0]} />*/}
     </Canvas>
   </>)
 }
--- src/Model.js
+++ src/Model.js
@@ -1,84 +1,37 @@
 import React, {useRef} from "react";
 import {useGLTF} from "@react-three/drei";
 import {useFrame} from "@react-three/fiber";
-import {l} from "./helpers"
 
-export const Model1 = (props) => {
-	const model = '/baseball_cap.glb';
-	// const model = '/projects/cap-configurator/baseball_cap.glb';
+export const Model = (props) => {
+	const model = '/baseball_cap.glb',
+	 { nodes, materials } = useGLTF(model),
+	 modelRef = useRef();
 	
-	const { nodes, materials } = useGLTF(model)
-	const modelRef = useRef()
-	props.onLoad()
-	
-	// useFrame((state, delta, clock) => {
 	useFrame(({ clock }) => {
 	  if(modelRef.current) {
 		  // modelRef.current.position.y = 10 * Math.sin(delta)
-		  modelRef.current.position.y = 2*Math.sin(clock.getElapsedTime())
+		  modelRef.current.rotation.y+= .01*Math.sin(clock.getElapsedTime())
+		  modelRef.current.position.y = 1.2*Math.sin(clock.getElapsedTime())
 	  }
 	})
-	// const c = 0xff0fe7
-	const c = 0xffffff
 	
 	const {
 		colorCrown,
 		colorBrim,
-		colorTop
+		colorTop,
+		colorStrip,
+		colorUnder,
 	} = props
 	
 	return (
 		<group ref={modelRef}  {...props} dispose={null}>
 			<group rotation={[-Math.PI / 2, 0, 0]}>
 				<group rotation={[Math.PI / 2, 0, 0]}>
-					{/*<mesh castShadow material-color="green" geometry={nodes.baseballCap.geometry} material={materials.baseballCap} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
 					<mesh castShadow material-color={colorTop} geometry={nodes.baseballCap.geometry} material={materials.baseballCap} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					{/*<mesh castShadow material-color={"salmon"} geometry={nodes.baseballCap_1.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
 					<mesh castShadow material-color={colorBrim} geometry={nodes.baseballCap_1.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					<mesh castShadow material-color="salmon" geometry={nodes.plastic.geometry} material={materials.plastic} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
-					<mesh castShadow material-color="orange" geometry={nodes.plastic_1.geometry} material={materials.plastic.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					<mesh castShadow material-color="aquamarine" geometry={nodes.baseballCap_2.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					<mesh castShadow material-color="purple" geometry={nodes.blinn1SG.geometry} material={materials.blinn1SG.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					{/*<mesh castShadow material-color={c} geometry={nodes.baseballCap_3.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
-					<mesh castShadow material-color={colorCrown} geometry={nodes.baseballCap_3.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-				</group>
-			</group>
-		</group>
-	)
-}
-
-
-export const Model2 = (props) => {
-	const model = '/baseball_cap2.glb';
-	// const model = '/projects/cap-configurator/baseball_cap2.glb';
-	const { nodes, materials } = useGLTF(model)
-	const modelRef = useRef()
-	props.onLoad()
-	
-	// useFrame(() => {
-	//   if(modelRef.current) modelRef.current.rotation.y += 0.01
-	// })
-	// const c = 0xff0fe7
-	const c = 0xffffff
-	const {
-		colorCrown,
-		colorBrim,
-		colorTop
-	} = props
-	
-	return (
-		<group ref={modelRef}  {...props} dispose={null}>
-			<group rotation={[-Math.PI / 2, 0, 0]}>
-				<group rotation={[Math.PI / 2, 0, 0]}>
-					{/*<mesh castShadow material-color="green" geometry={nodes.baseballCap.geometry} material={materials.baseballCap} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
-					<mesh castShadow material-color={colorTop} geometry={nodes.baseballCap.geometry} material={materials.baseballCap} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					{/*<mesh castShadow material-color={"salmon"} geometry={nodes.baseballCap_1.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
-					<mesh castShadow material-color={colorBrim} geometry={nodes.baseballCap_1.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					<mesh castShadow material-color="salmon" geometry={nodes.plastic.geometry} material={materials.plastic} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
-					<mesh castShadow material-color="orange" geometry={nodes.plastic_1.geometry} material={materials.plastic.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					<mesh castShadow material-color="aquamarine" geometry={nodes.baseballCap_2.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					<mesh castShadow material-color="purple" geometry={nodes.blinn1SG.geometry} material={materials.blinn1SG.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
-					{/*<mesh castShadow material-color={c} geometry={nodes.baseballCap_3.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
+					<mesh castShadow material-color={colorStrip} geometry={nodes.plastic.geometry} material={materials.plastic} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />*/}
+					<mesh castShadow material-color={colorStrip} geometry={nodes.plastic_1.geometry} material={materials.plastic.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
+					<mesh castShadow material-color={colorUnder} geometry={nodes.baseballCap_2.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
 					<mesh castShadow material-color={colorCrown} geometry={nodes.baseballCap_3.geometry} material={materials.baseballCap.clone()} position={[0.005, -2.9, -11.842]} rotation={[-0.161, 0, 0]} scale={20.118} />
 				</group>
 			</group>
